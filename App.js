22d97f0366e63cdfd1566e3f1e04acf28cc81ab2
DELESTRE Pierre EXT <pierre.delestre-prestataire@laposte.fr>
2025-08-12 15:04:27 +0100

    Builds GoalInput component

--- App.js
+++ App.js
@@ -1,23 +1,17 @@
 import { useState } from 'react';
 import { 
-  Button,
   StyleSheet,
-  TextInput,
   View,
   FlatList
  } from 'react-native';
 
  import GoalItem from './components/GoalItem';
+ import GoalInput from './components/GoalInput';
 
 export default function App() {
-  const [enteredGoalText, setEnteredGoalText] = useState('');
   const [courseGoals, setCourseGoals] = useState([]);
 
-  function goalInputHandler(enteredText) {
-    setEnteredGoalText(enteredText);
-  };
-
-  function addGoalHandler() {
+  function addGoalHandler(enteredGoalText) {
     setCourseGoals((currentCourseGoals) => 
       [...currentCourseGoals, 
         {text: enteredGoalText, key: Math.random().toString()}
@@ -26,15 +20,7 @@ export default function App() {
 
   return (
     <View style={styles.appContainer}>
-      <View style={styles.inputContainer}>
-        <TextInput 
-          placeholder='Your course goal!' 
-          style={styles.textInput}
-          onChangeText={goalInputHandler} />
-        <Button 
-          title='Add Goal'
-          onPress={addGoalHandler} />
-      </View>
+      <GoalInput onAddGoal={addGoalHandler} />
       <View style={styles.goalsContainer}>
         <FlatList data={courseGoals} renderItem={(itemData) => {
           return <GoalItem text={itemData.item.text} />;
@@ -50,22 +36,6 @@ const styles = StyleSheet.create({
     flex: 1,
     paddingTop: 50,
     paddingHorizontal: 16,
-  },
-  inputContainer: {
-    flex: 1,
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    alignItems: 'center',
-    marginBottom: 24,
-    borderBottomWidth: 1,
-    borderBottomColor: '#cccccc',
-  },
-  textInput: {
-    borderWidth: 1,
-    borderColor: '#cccccc',
-    width: '70%',
-    marginRight: 8,
-    padding: 8
   },
   goalsContainer: {
     flex: 5
